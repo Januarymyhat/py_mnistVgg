e84358a9b0c993354274cf4854c88fd2291c0c01
myhat <103307000+Januarymyhat@users.noreply.github.com>
2023-03-31 14:46:18 +0800

    Update vgg_ui.py

--- vgg_ui.py
+++ vgg_ui.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 from keras.models import load_model
 import numpy as np
 from PyQt5.QtCore import Qt, QSize
@@ -9,13 +12,14 @@ from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QLabel
 from keras.utils import image_utils
 
-import paintBoard
+import paint_board
+import target_detection
 
 
-class MainWidget(QWidget):
+class VGGUI(QWidget):
 
     def __init__(self):
-        super(MainWidget, self).__init__()
+        super(VGGUI, self).__init__()
 
         self.img = None
         self.setFixedSize(800, 950)  # 设置窗口宽高
@@ -31,7 +35,7 @@ class MainWidget(QWidget):
         # )
 
         # 添加一系列控件
-        self.paintBoard = paintBoard.PaintBoard(self)
+        self.paintBoard = paint_board.PaintBoard(self)
         # 上传文件显示的board
         self.label_showBoard = QLabel(self)
 
@@ -55,7 +59,7 @@ class MainWidget(QWidget):
 
         self.btn_clear = QPushButton("Clear", self)
         self.btn_clear.setGeometry(350, 810, 80, 35)
-        self.btn_clear.clicked.connect(self.btn_clear_on_clicked) #绑定多个函数
+        self.btn_clear.clicked.connect(self.btn_clear_on_clicked)  # 绑定多个函数
 
         self.cbtn_eraser = QCheckBox("Eraser")
         self.cbtn_eraser.setParent(self)
@@ -85,12 +89,25 @@ class MainWidget(QWidget):
         self.__comboBox_penColor = QComboBox(self)
         self.__comboBox_penColor.setGeometry(380, 860, 80, 35)
         self.fillColorList(self.__comboBox_penColor)  # 用各种颜色填充下拉列表
-        self.__comboBox_penColor.currentIndexChanged.connect(lambda: self.on_PenColorChange())  # 关联下拉列表的当前索引变更信号与函数on_PenColorChange
+        self.__comboBox_penColor.currentIndexChanged.connect(
+            lambda: self.on_PenColorChange())  # 关联下拉列表的当前索引变更信号与函数on_PenColorChange
 
         self.__btn_close = QPushButton("Close", self)
         self.__btn_close.setGeometry(530, 860, 100, 35)
         self.__btn_close.clicked.connect(self.btn_close_on_clicked)
         # self.model = load_model('test/model.h5')
+
+        self.img_number = 0
+        self.img_processed_dir = 'test/detection/'
+        self.__btn_back = QPushButton("Back", self)
+        self.__btn_back.setGeometry(310, 900, 80, 35)
+        self.__btn_back.clicked.connect(self.btn_back_on_click)
+        self.__btn_back.hide()
+
+        self.__btn_next = QPushButton("Next", self)
+        self.__btn_next.setGeometry(420, 900, 80, 35)
+        self.__btn_next.clicked.connect(self.btn_next_on_click)
+        self.__btn_next.hide()
 
     # 关闭按钮的功能：关闭窗口
     def btn_close_on_clicked(self):
@@ -132,7 +149,7 @@ class MainWidget(QWidget):
             self.img = self.paintBoard.GetContentAsQImage()
             self.img.save(r'test/1.png')
             self.img = image_utils.load_img('test/1.png', target_size=(32, 32))
-        
+
         predict = self.recognize_result(self.img)
         self.label_result.setText(str(predict[0]))  # 显示识别结果
 
@@ -151,6 +168,20 @@ class MainWidget(QWidget):
         prediction = np.argmax(prediction, axis=1)
         return prediction
 
+    # 清除上传的图片
+    def btn_clear_on_clicked(self):
+        # self.label_showBoard.setPixmap(QPixmap(""))
+        if self.paintBoard.isEmpty:
+            self.label_showBoard.setPixmap(QPixmap(""))
+            self.label_showBoard.hide()
+        else:
+            self.paintBoard.Clear()
+
+    def show_img(self):
+        image = QPixmap(self.img_processed_dir + str(self.img_number) + '.png').scaled(800, 800)
+        self.label_showBoard.setPixmap(image)
+        self.img = ImageQt.fromqpixmap(image)
+
     def btn_file_on_click(self):
         # file_image = QFileDialog.getOpenFileName(self, "File", "C:\\", 'Image files (*.jpg *.png *.jpeg)')
         # self.paintBoard.__board = QPixmap(file_image)
@@ -162,15 +193,30 @@ class MainWidget(QWidget):
         if fiDir == '':
             pass
         else:
-            image = QPixmap(fiDir).scaled(800, 800)
-            self.label_showBoard.setPixmap(image)
-            self.img = ImageQt.fromqpixmap(image)
+            # 清空文件夹
+            shutil.rmtree(self.img_processed_dir)
+            os.mkdir(self.img_processed_dir)
+            # 开始目标检测
+            target_detection.target_detect(fiDir)
 
-    #清除长传的图片
-    def btn_clear_on_clicked(self):
-        # self.label_showBoard.setPixmap(QPixmap(""))
-        if self.paintBoard.isEmpty:
-            self.label_showBoard.setPixmap(QPixmap(""))
-            return self.label_showBoard.hide()
-        else:
-            return self.paintBoard.Clear()
+            if not os.path.exists(self.img_processed_dir + '1.png'):
+                pass
+            else:
+                self.__btn_next.show()
+            self.show_img()
+
+    def btn_back_on_click(self):
+        self.__btn_next.show()
+        self.img_number -= 1
+        if self.img_number == 0:
+            self.__btn_back.hide()
+        self.show_img()
+
+    def btn_next_on_click(self):
+        self.__btn_back.show()
+        self.img_number += 1
+        images = len(os.listdir(self.img_processed_dir))
+        if self.img_number == images - 1:
+            self.__btn_next.hide()
+        self.show_img()
+
